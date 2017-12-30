5a631ccbb05e20847c2e9775a9dca6490127df50
EllaVader <jroupon@yahoo.com>
2017-12-30 11:45:17 -0500

    Learning about Actions for Redux state

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,10 +1,42 @@
 import { createStore } from 'redux';
 
 // createStore takes a fn as a first argument.  
-// argument to this function is the state.  
+// 1st argument to this function is the state, 2nd argument is the action being performed on the state
 // There is no constructor function to set up the default, so set you state props with default values.
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  
+  switch(action.type){
+    case 'INCREMENT' : 
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default: 
+      return state;
+  }
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
 });
 
 console.log(store.getState());
