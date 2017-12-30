274eb26f9e1ed2f043070f1d3489290a802e42bf
EllaVader <jroupon@yahoo.com>
2017-12-30 13:12:19 -0500

    ES6 Destructuring examples

---
+++ src/playground/destructuring.js
@@ -0,0 +1,42 @@
+// const person = {
+//   name: "Janine",
+//   age: 47,
+//   location: {
+//     city: 'Glenmoore',
+//     temp: 28
+//   }
+// };
+
+// // we can pull out the properties we want off of the object without having to reference them via the object
+// // const name = person.name;
+// // const age = person.age;
+
+// // es6 destructuring - right side of = is the object, left side are the props we want from it.
+// // set up a default
+// const { name: firstName = 'no name', age } = person;
+
+// console.log(`${firstName} is ${age}`);
+
+// // nested object destructuring example
+// // const {city, temp} = person.location;
+// // if (city && temp){
+// //   console.log(`It's ${temp} in ${city}.`);
+// // }
+
+// // you can rename the variable of the object we are destructuring
+// const { city, temp: temperature } = person.location;
+// console.log(`Temp is: ${temperature}`);
+
+// // you can set a default value 
+
+const book = {
+  title: 'Ego the Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin'
+  }
+};
+
+const { name: publisherName = 'Self-Published' } = book.publisher;
+
+console.log(publisherName);
