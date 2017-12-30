a8680a690c44483bfe72f241ed019ef9f4c43404
EllaVader <jroupon@yahoo.com>
2017-12-30 13:44:54 -0500

    Array destructuring

---
+++ src/playground/array-destructuring.js
@@ -0,0 +1,5 @@
+//const address = ['120 Shea Lane', 'Glenmoore', 'PA', '19343'];
+
+const item = ['coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const  [mediumHot, ,medium] = item;
+console.log(`A medium ${mediumHot} costs ${medium}`)
