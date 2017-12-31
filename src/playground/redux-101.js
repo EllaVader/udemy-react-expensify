afcc71057eaca407b32fdecdbf84c72267c04c82
EllaVader <jroupon@yahoo.com>
2017-12-31 15:28:02 -0500

    Refactored redux-101 to use object destrcuturing and Action Generator methods

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,20 +1,43 @@
 import { createStore } from 'redux';
 
+// Action generators - functions that return action objects
+// destructure the object and set default to 1 if not set.
+// build the action object by using this method.
+const incrementCount = ({ incrementBy = 1} = {}) => ({
+  // default the payload to an empty object.
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1} = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
 // createStore takes a fn as a first argument.  
-// 1st argument to this function is the state, 2nd argument is the action being performed on the state
+// 1st argument to this function is the state, 2nd argument is the action being performed on the state.
+// The action argument is passed in when store.dispatch is called, it passes in the action to dispatch.
+// this implictly calls createStore with the Action as the 2nd argument.
 // There is no constructor function to set up the default, so set you state props with default values.
+// This method returns the updated store object to be used in your application.
 const store = createStore((state = { count: 0 }, action) => {
-  
   switch(action.type){
     case 'INCREMENT' : 
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case 'SET' : {
       return { 
@@ -23,7 +46,7 @@ const store = createStore((state = { count: 0 }, action) => {
     };
     case 'RESET':
       return {
-        count: 0
+       count: 0
       };
     default: 
       return state;
@@ -34,29 +57,14 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+store.dispatch(incrementCount({incrementBy: 5}));
 
-store.dispatch({
-  type: 'DECREMENT'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(setCount({count: 100}));
