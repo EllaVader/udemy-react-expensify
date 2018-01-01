a393978bcdc446b3939872da8df70ad47ef7913a
EllaVader <jroupon@yahoo.com>
2018-01-01 10:27:23 -0500

    Adding more reducers and action generators. Also using ES6 spread operator

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,26 +1,59 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
-//  Some things Actions we want to create:
-// ADD_EXPENSE
-// REMOVE_EXPENSE
-// EDIT_EXPENSE
-// SET_TEXT_FILTER
-// SORT_BY_DATE
-// SORT_BY_AMOUNT
-// SET_START_DATE
-// SET_END_DATE
+//  ACTION GENERATORS:
+// 1. ADD_EXPENSE
+// remember: Action generators take in a function that is called when the dispatch is 
+// made to set the values of the action object.
+const addExpense = (
+  { 
+    description = '', 
+    note = '', 
+    amount = 0, 
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    // use the uuid library to generate a unique id 'yarn add uuid'
+    // using the JSON shorthand to set the other properties
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
 
-// Expenses Reducer
+// 2. REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+// 3. EDIT_EXPENSE
+// 4. SET_TEXT_FILTER
+// 5. SORT_BY_DATE
+// 6. SORT_BY_AMOUNT
+// 7. SET_START_DATE
+// 8. SET_END_DATE
+
+// REDUCERS:
+// 1. Expenses Reducer
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch(action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense]
+    case 'REMOVE_EXPENSE':
+      return state.filter(({id}) => id !== action.id)
     default:
       return state;
   }
 };
 
-// Filter Reducer
+// 2. Filter Reducer
 const filterReducerDefaultState = {
   text: '',
   sortBy: 'date',
@@ -42,8 +75,16 @@ const store = createStore(
   })
 );
 
-// return the state of our store.
-console.log(store.getState());
+store.subscribe(() => 
+  // return the state of our store.
+  console.log(store.getState())
+);
+
+const expense1 = store.dispatch(addExpense({ description: 'rent', amount: 100 }));
+const expense2 = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+store.dispatch(removeExpense({id: expense1.expense.id}));
+
 
 // data we want to track state on
 const demoState = {
@@ -61,5 +102,3 @@ const demoState = {
     endDate: undefined
   }
 };
-
-// now we can write complex reducers for this complex state.
