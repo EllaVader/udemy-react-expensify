888d8f7c23394f3241da462b99b88d26afd4df23
EllaVader <jroupon@yahoo.com>
2018-01-04 15:06:59 -0500

    adding sort by date/amount select dropdown

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { setTextFilter, sortByDate, sortByAmount } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
@@ -12,17 +12,15 @@ import './styles/styles.scss'
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 1500 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 2500 }));
-store.dispatch(setTextFilter('bill'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('water'));
-}, 3000);
+store.dispatch(addExpense({ description: 'Water Bill', amount: 1500, createdAt: 500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 2500, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 10950, createdAt: 700 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
+
+//store.dispatch(sortByAmount());
 
 // wire up the store with the provider.
 const jsx = (
