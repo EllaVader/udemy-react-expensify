c8ec9ae892ab885fc0ab5a17acfb808c5dcf232a
EllaVader <jroupon@yahoo.com>
2018-01-28 11:56:21 -0500

    Adding configurations for prod and dev versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // define entry point - path to our main driver file
 // define output of final bundle file - path to where the public version of the file will live.
@@ -7,25 +8,51 @@ const path = require('path');
 //use node path to build the path to where we want to put the output file
 
 // module.exports a way to expose an object to another file via node
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  // create new instance of this class, argument is the name of the file our 
+  // css will get extracted into
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
         test: /\.s?css$/,
-    }]
-  },
-  devtool: 'cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
+        use: CSSExtract.extract({
+         use: [
+           {
+             loader: 'css-loader',
+             options: {
+               sourceMap: true
+             }
+           },
+           {
+             loader: 'sass-loader',
+             options: {
+               sourceMap: true
+             }
+           }
+         ]
+        })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map': 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
   }
-};
+}
