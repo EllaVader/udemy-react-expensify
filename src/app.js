cb8c1f31343eb2e1d0a53bfe8cb3b3c1e2420692
EllaVader <jroupon@yahoo.com>
2018-01-28 01:09:09 -0500

    Commented out the dispatches

--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 1500, createdAt: 500 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 2500, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 10950, createdAt: 700 }));
+// store.dispatch(addExpense({ description: 'Water Bill', amount: 1500, createdAt: 500 }));
+// store.dispatch(addExpense({ description: 'Gas Bill', amount: 2500, createdAt: 1000 }));
+// store.dispatch(addExpense({ description: 'Rent', amount: 10950, createdAt: 700 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
